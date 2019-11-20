6dfde37c4b1f53f2745cd6578c746a816da4f6af
Bruno Meilick <b@bnomei.com>
2019-11-20 18:02:09 +0000

    :bang: moved content => storage/content
    Signed-off-by: Bruno Meilick <b@bnomei.com>

--- public/index.php
+++ public/index.php
@@ -6,9 +6,9 @@ $kirby = new Kirby([
     'roots' => [
         'index'    => __DIR__,
         'base'     => $base    = dirname(__DIR__),
-        'content'  => $base . '/content',
         'site'     => $base . '/site',
         'storage'  => $storage = $base . '/storage',
+        'content'  => $storage . '/content',
         'accounts' => $storage . '/accounts',
         'cache'    => $storage . '/cache',
         'sessions' => $storage . '/sessions',
